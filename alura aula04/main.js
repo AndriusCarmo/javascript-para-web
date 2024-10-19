77bd92a9dbf65bf8dae0fd92d55ddf079e41f188
AndriusCarmo <amjc2@poli.br>
2024-10-19 01:31:24 -0300

    Update main.js
    Melhorando o código

--- alura aula04/main.js
+++ alura aula04/main.js
@@ -1,5 +1,13 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento =document.querySelector(seletorAudio);
+    
+    if( elemento != null && elemento.localName == 'audio'){
+        elemento.play();
+    }
+    else{
+        alert('Elemento não encontrado ou seletor inválido')
+    }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -14,8 +22,12 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
-    tecla.onkeydown = function() { 
-        tecla.classList.add('ativa');
+    tecla.onkeydown = function(evento) { 
+        
+        if(evento.code == 'Space' || evento.code == 'Enter'){      
+            tecla.classList.add('ativa'); // adiciona a classe 'ativa' nalista de classes                       
+        }                                 // apenas as teclas enter e espaço
+
     }    
     tecla.onkeyup = function() {
         tecla.classList.remove('ativa');
